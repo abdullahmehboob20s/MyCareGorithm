93f8f9b10213c94fc329dadc6c0f8b73da947d64
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2021-09-11 12:08:34 +0500

    s

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,3 +1,4 @@
+const navbar_wrapper = document.querySelector(".navbar_wrapper");
 const navbar_links = document.querySelector(".navbar_links");
 const navbar_hamburger = document.querySelector(".navbar_hamburger");
 const navbar_links_menue = document.querySelector(".navbar_links_menue");
@@ -16,4 +17,20 @@ navbar_links_menue.addEventListener("click", () => {
 black_screen.addEventListener("click", () => {
   navbar_links.classList.remove("open");
   black_screen.classList.remove("show");
+});
+
+let lastScroll = 0;
+
+document.addEventListener("scroll", (e) => {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (scrollTop > 400) {
+    if (scrollTop > lastScroll) {
+      navbar_wrapper.style.top = "-93px";
+      // navbar_wrapper.style.background = "rgb(244, 202, 76)";
+    } else {
+      navbar_wrapper.style.top = "0";
+    }
+    lastScroll = scrollTop;
+  }
 });
